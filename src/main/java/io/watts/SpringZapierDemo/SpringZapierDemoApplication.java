36714414bba372b76a0ee82925bc67e9dddcc8e4
iyasuwatts <iyasuwatts@iyasuwattss-MacBook-Pro.local>
2017-11-16 17:42:57 -0500

    [setter/getter annotations], [mysql support], [get person by id]

--- src/main/java/io/watts/SpringZapierDemo/SpringZapierDemoApplication.java
+++ src/main/java/io/watts/SpringZapierDemo/SpringZapierDemoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class SpringZapierDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/io/watts/SpringZapierDemo/model/Person.java
+++ src/main/java/io/watts/SpringZapierDemo/model/Person.java
@@ -1,9 +1,9 @@
 package io.watts.SpringZapierDemo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 /**
  * Created by iyasuwatts on 11/8/17.
@@ -13,50 +13,36 @@ import javax.persistence.Table;
 public class Person {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    @Setter
+    @Getter
     private Integer id;
+
+    @Column(name = "first_name")
+    @Setter
+    @Getter
     private String firstName;
+
+    @Column(name = "last_name")
+    @Setter
+    @Getter
     private String lastName;
+
+    @Column(name = "phone")
+    @Setter
+    @Getter
     private String phone;
+
+    @Column(name = "email")
+    @Setter
+    @Getter
     private String email;
 
-    public Integer getId() {
-        return id;
+
+    public Person(){
+
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
